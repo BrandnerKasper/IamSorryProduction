de0de15e4be01a95f0611fa686304273d54def30
brauney <andreas.braun1@stud-mail.uni-wuerzburg.de>
2021-03-09 11:17:33 +0100

    Empty Unreal Project. I AM SORRY MAAAAN!!!!

---
+++ Source/IAmSorryONE/IAmSorryONEGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IAmSorryONEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IAMSORRYONE_API AIAmSorryONEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
